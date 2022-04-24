ccc4a34f7ef5c018e45fd70878d5a1f51704a90a
christopher <cdnance1829@gmail.com>
2022-04-24 00:53:48 -0400

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,15 +1,40 @@
 // 1. Write your functions here
 
+function line(array) {
+    if (array.length === 0) {
+        retrun`The line is currently empty.`
+    }
+
+    let sum = 0;
+    let result = "";
+    for (let i = 1; i < array.length + 1; i++) {
+
+        result += `The line is currently: ${i}. ${array[sum++]} `;
+        
+    }
+        return result;
+}
+
+function takeANumber(array, names) {
+    let nextInLine = "";
+    let count = 0;
+    for (let i = 1; i < array.length + 1; i++) {
+
+        nextInLine += console.log(`Welcome, ${array[count]}. You are number ${i} in line.`)
+        count++;
+    }
+    return nextInLine;
+}
 
 // 2. Example Usage
 
-// const katzDeli = []
+const katzDeli = ['Ada', 'Johnny']
 
 // takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
 // takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
 // takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+console.log(line(katzDeli)) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
 // nowServing(katzDeli) //=> "Currently serving Ada."
 
